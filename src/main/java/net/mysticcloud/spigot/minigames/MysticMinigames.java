7836d4685b808230a6fa09fa6ff77b23c040f79a
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-11-24 18:06:02 -0600

    init commit

---
+++ src/main/java/net/mysticcloud/spigot/minigames/MysticMinigames.java
@@ -0,0 +1,20 @@
+package net.mysticcloud.spigot.minigames;
+
+
+import net.mysticcloud.spigot.core.utils.CoreUtils;
+import net.mysticcloud.spigot.core.utils.MessageUtils;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class MysticMinigames extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}

8ec1a61e492c3899d84f4d6a555e62b2281da05a
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-11-30 19:04:01 -0600

    Chat \#release

--- src/main/java/net/mysticcloud/spigot/minigames/listeners/ChatListener.java
+++ src/main/java/net/mysticcloud/spigot/minigames/listeners/ChatListener.java
@@ -3,6 +3,7 @@ package net.mysticcloud.spigot.minigames.listeners;
 import net.mysticcloud.spigot.core.utils.MessageUtils;
 import net.mysticcloud.spigot.minigames.MysticMinigames;
 import net.mysticcloud.spigot.minigames.utils.Game;
+import net.mysticcloud.spigot.minigames.utils.Team;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -17,9 +18,9 @@ public class ChatListener implements Listener {
         if (e.getPlayer().hasMetadata("game")) {
             Game game = (Game) e.getPlayer().getMetadata("game").get(0).value();
             assert game != null;
-            if (!game.getGameState().hasStarted()) {
-                game.sendMessage(MessageUtils.colorize("&3" + e.getPlayer().getName() + "&e: ") + e.getMessage());
-            }
+            Team team = game.getPlayers().get(e.getPlayer().getUniqueId());
+            game.sendMessage(MessageUtils.colorize("&3" + (team.equals(Team.NONE) ? "" : team.chatColor() + "[&l" + team.name() + "&r" + team.chatColor() + "] ") + e.getPlayer().getName() + "&e: ") + e.getMessage());
+
             e.setCancelled(true);
         }
     }

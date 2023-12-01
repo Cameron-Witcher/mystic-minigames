02af308b68e33db832da83b24ab1ab2a54dd613b
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-12-01 05:32:05 -0600

    flags?? \#release

--- src/main/java/net/mysticcloud/spigot/minigames/listeners/DeathListener.java
+++ src/main/java/net/mysticcloud/spigot/minigames/listeners/DeathListener.java
@@ -7,13 +7,17 @@ import net.mysticcloud.spigot.minigames.utils.Team;
 import net.mysticcloud.spigot.minigames.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.metadata.FixedMetadataValue;
+
+import java.util.UUID;
 
 public class DeathListener implements Listener {
     public DeathListener(MysticMinigames plugin) {
@@ -23,7 +27,11 @@ public class DeathListener implements Listener {
     @EventHandler
     public void onPlayerDamage(EntityDamageByEntityEvent e) {
         if (e.getEntity() instanceof Player && e.getEntity().hasMetadata("game")) {
-            e.getEntity().setMetadata("last_damager", new FixedMetadataValue(Utils.getPlugin(), e.getDamager().getUniqueId()));
+            UUID damager = e.getDamager().getUniqueId();
+            if(e.getDamager() instanceof Projectile && ((Projectile)e.getDamager()).getShooter() instanceof LivingEntity){
+                damager = ((LivingEntity)((Projectile)e.getDamager()).getShooter()).getUniqueId();
+            }
+            e.getEntity().setMetadata("last_damager", new FixedMetadataValue(Utils.getPlugin(), damager));
             Bukkit.getScheduler().runTaskLater(Utils.getPlugin(), () -> {
                 e.getEntity().removeMetadata("last_damager", Utils.getPlugin());
             }, 5 * 20);

54d48c65d12bae3ac57324fb1bc169e1a1a402f6
Cameron-Witcher <CameronWitcher1@gmail.com>
2023-12-02 02:35:09 -0600

    World stores game data now \#release

--- src/main/java/net/mysticcloud/spigot/minigames/listeners/BlockListener.java
+++ src/main/java/net/mysticcloud/spigot/minigames/listeners/BlockListener.java
@@ -4,24 +4,10 @@ import net.mysticcloud.spigot.core.utils.CoreUtils;
 import net.mysticcloud.spigot.core.utils.MessageUtils;
 import net.mysticcloud.spigot.minigames.MysticMinigames;
 import net.mysticcloud.spigot.minigames.utils.Game;
-import net.mysticcloud.spigot.minigames.utils.Team;
-import net.mysticcloud.spigot.minigames.utils.Utils;
-import net.mysticcloud.spigot.minigames.utils.games.CTW;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockFromToEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
-import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.event.block.*;
 import org.bukkit.metadata.MetadataValue;
 
 public class BlockListener implements Listener {
@@ -35,8 +21,72 @@ public class BlockListener implements Listener {
             for (MetadataValue value : e.getBlock().getWorld().getMetadata("game")) {
                 Game game = (Game) value.value();
                 for (Location location : game.getNoBuildZones())
+                    if (CoreUtils.distance(location, e.getBlock().getLocation()) <= 5) e.setCancelled(true);
+
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockChange(BlockBreakEvent e) {
+        if (e.getBlock().getWorld().hasMetadata("game")) {
+            for (MetadataValue value : e.getBlock().getWorld().getMetadata("game")) {
+                Game game = (Game) value.value();
+                for (Location location : game.getNoBuildZones())
                     if (CoreUtils.distance(location, e.getBlock().getLocation()) <= 5) {
                         e.setCancelled(true);
+                        e.getPlayer().sendMessage(MessageUtils.colorize("&3Sorry, you can't edit blocks right here."));
+                    }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockChange(BlockPlaceEvent e) {
+        if (e.getBlock().getWorld().hasMetadata("game")) {
+            for (MetadataValue value : e.getBlock().getWorld().getMetadata("game")) {
+                Game game = (Game) value.value();
+                for (Location location : game.getNoBuildZones())
+                    if (CoreUtils.distance(location, e.getBlock().getLocation()) <= 5) {
+                        e.setCancelled(true);
+                        e.getPlayer().sendMessage(MessageUtils.colorize("&3Sorry, you can't edit blocks right here."));
+                    }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockChange(BlockExplodeEvent e) {
+        if (e.getBlock().getWorld().hasMetadata("game")) {
+            for (MetadataValue value : e.getBlock().getWorld().getMetadata("game")) {
+                Game game = (Game) value.value();
+                for (Location location : game.getNoBuildZones())
+                    if (CoreUtils.distance(location, e.getBlock().getLocation()) <= 5) e.setCancelled(true);
+
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockChange(BlockBurnEvent e) {
+        if (e.getBlock().getWorld().hasMetadata("game")) {
+            for (MetadataValue value : e.getBlock().getWorld().getMetadata("game")) {
+                Game game = (Game) value.value();
+                for (Location location : game.getNoBuildZones())
+                    if (CoreUtils.distance(location, e.getBlock().getLocation()) <= 5) e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockChange(BlockIgniteEvent e) {
+        if (e.getBlock().getWorld().hasMetadata("game")) {
+            for (MetadataValue value : e.getBlock().getWorld().getMetadata("game")) {
+                Game game = (Game) value.value();
+                for (Location location : game.getNoBuildZones())
+                    if (CoreUtils.distance(location, e.getBlock().getLocation()) <= 5) {
+                        e.setCancelled(true);
+                        e.getPlayer().sendMessage(MessageUtils.colorize("&3Sorry, you can't edit blocks right here."));
                     }
             }
         }
